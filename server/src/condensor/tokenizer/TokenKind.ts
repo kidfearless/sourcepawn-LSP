b8ac5a0740e524590f743bc5b902a9e68ab5234a
KiD Fearless <kidfearless1@gmail.com>
2019-11-06 09:47:06 -0700

    Create TokenKind.ts

---
+++ server/src/condensor/tokenizer/TokenKind.ts
@@ -0,0 +1,29 @@
+﻿export enum TokenKind
+{
+	Identifier,				//done
+	Number,					//d
+	Character,				//d
+	BraceOpen,				//d
+	BraceClose,				//d
+	ParenthesisOpen,		//d
+	ParenthesisClose,		//d
+	Quote,					//d
+	SingleLineComment,		//d
+	MultiLineComment,		//d
+	Semicolon,				//d
+	Comma,					//d
+	Assignment,				//d
+	
+	FunctionIndicator,		//d
+	Constant,				//d
+	Enum,					//d
+	Struct,					//d
+	MethodMap,				//d
+	Property,				//d
+	PrePocessorDirective,	//d
+	TypeDef,				//d
+	TypeSet,				//d
+
+	EOL,					//d
+	EOF,					//d
+}
